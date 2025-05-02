8b4bfe96f95c907bcf59deed6667c3251efb5860
Cheng <45914566+Justype@users.noreply.github.com>
2025-05-02 12:38:52 -0400

    Update utils-hca.R
    fix R 4.3 class issue

--- R/utils-hca.R
+++ R/utils-hca.R
@@ -78,7 +78,7 @@
 
     res = c()
 
-    if (class(x) == 'matrix') {
+    if (any(class(x) == 'matrix')) {
         x = .hca_cor(x, method = cor.method)
         res = c(res, list(cr = x))
         if (cor.end) return(res)
